76ce44ce02ca6d4c41f563710e6c552fdca53a4f
tan nguyen <bivani2805@gmail.com>
2024-10-26 06:45:26 +0000

    add background for game play

--- fighting-game/src/App.js
+++ fighting-game/src/App.js
@@ -1,5 +1,5 @@
 import Idle from './idle/Idle.png';
-import skill_1 from './skills/skill_1.png'
+import skill from './skills/skill.png'
 import './App.css';
 import React from 'react';
 
@@ -8,8 +8,9 @@ class App extends React.Component{
     super(props)
     this.state = { // USER TO PASS INFORMATION FROM OUTSIDE AND AROUND METHODS
       imageSrc: Idle,
-      skill: skill_1,
+      skill: skill,
       status: 'idle-stand',
+      isPlayerTurn: false,
     };
     this.attack = this.attack.bind(this);
     this.gameScreen = this.gameScreen.bind(this);
@@ -26,12 +27,13 @@ class App extends React.Component{
 
   gameScreen() {
     document.getElementById("screen-one").style.display = "block";
+    document.getElementById("screen-one").className = "screen-one-background ";
     document.getElementById("menu").style.display = "none";
   }
 
   menuScreen() {
     document.getElementById("screen-one").style.display = "none";
-    document.getElementById("menu").style.display = "block";
+    document.getElementById("menu").style.display = "flex";
   }
 
   render() {
@@ -39,14 +41,16 @@ class App extends React.Component{
       <div className="App">
         <div id="menu">
           <button className="play-btn btn btn-primary btn-lg" onClick={this.gameScreen}>Play</button>
+          <button className="instruction-btn btn btn-primary btn-lg">Instruction</button>
         </div>
         <div id="screen-one">
-          <button className="play-btn btn btn-primary btn-lg" onClick={this.menuScreen}>Play</button>
-          <h1>Animation</h1>
+          <button className="back-btn btn btn-primary btn-lg" onClick={this.menuScreen}>Back</button>
+          <h1>Match One</h1>
           <div className='character-container'>
             <div id="container-button">
               <button className='style-button' onClick={this.attack}>Attack</button>
               <button className='style-button' onClick={this.attack2}>Attack2</button>
+              <button className='style-button' onClick={this.attack2}>Defense</button>
             </div>
             <img id="character" className={this.state.status} src={this.state.imageSrc} alt="character"/>
             <img id="skill" src={this.state.skill} alt="skill"/>
